334e6ffb4a21f84225ab742607bddb3ebf2dc51b
nabab <thomas.nabet@gmail.com>
2025-04-16 01:57:57 +0200

    Menu working

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,5 +1,6 @@
 (() => {
   return {
+    mixins: [bbn.cp.mixins.basic],
     props: {
       source: {
         type: Array,
@@ -16,9 +17,44 @@
       return {
       }
     },
+    computed: {
+      listSource() {
+        const cp = this;
+        return [
+          {
+            text: bbn._("New tag"),
+            value: '',
+            action(a) {
+              cp.getPopup({
+                label: false,
+                component: 'appui-tag-form',
+                source: {
+                  tag: '',
+                  description: '',
+                  color: ''
+                },
+                events: {
+                  success() {
+                    bbn.fn.log("SUCCESS")
+                  },
+                  add() {
+                    bbn.fn.log("ADD")
+                  }
+                }
+              })
+            }
+          },
+          ...this.tags.map(a => {
+            const r = JSON.parse(JSON.stringify(a));
+            r.action = a => cp.$emit('addtag', a);
+            return r;
+          })
+        ]
+      }
+    },
     methods: {
       renderElement(ele) {
-        return '<div class="bbn-spadding bbn-block" style="color: ' + ele.value + '">' + ele.text + '</div>';
+        return '<div class="bbn-block bbn-vxspadding bbn-hspadding" style="color: ' + ele.value + '">' + ele.text + '</div>';
       },
       selectTag() {
         bbn.fn.log('selectTag', arguments)
